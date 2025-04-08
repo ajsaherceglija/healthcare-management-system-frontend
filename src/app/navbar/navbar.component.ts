bb587001338645c006844685172d4a4f8133efd8
ajsaherceglija <ajsa.herceglija@gmail.com>
2025-04-09 01:59:04 +0200

    Add navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterModule} from '@angular/router';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {NgIf} from '@angular/common';
+import {AuthService} from '../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatIconModule, MatListModule, RouterModule, NgIf],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit{
+  public userId!: number;
+  public isDoctor!: boolean;
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    // Subscribe to the user ID from the service
+    this.authService.currentUser$.subscribe((user) => {
+      this.userId = user?.uid!;
+      this.isDoctor = this.authService.isDoctor();
+    });
+  }
+  logout() {
+    this.authService.logout();
+
+    // Redirect the user to the login page (or another page)
+    this.router.navigate(['/login']);
+  }
+}
