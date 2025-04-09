08991871bcb88f9c8946a9e9155e9b3003acb6b8
adnaja01 <adnaja011@gmail.com>
2025-04-09 23:56:47 +0200

    update transfer page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {ProfileComponent} from './profile/profile.component';
+import {DocumentsComponent} from './documents/documents.component';
 
 export const routes: Routes = [
-  {path: 'user/:uid', component: ProfileComponent}
+  {path: 'user/:uid', component: ProfileComponent},
+  { path: 'documents/:uid', component: DocumentsComponent }
 ];
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { DocumentSectionComponent } from './document-section/document-section.component';
+import { User } from '../models/user.model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-documents',
-  imports: [],
+  standalone: true,
+  imports: [DocumentSectionComponent, NgIf],
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css'],
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit, OnChanges {
+  user!: User;
+  finishedDocuments: { name: string }[] = [];
+  myDocuments: { name: string }[] = [];
 
+  constructor(private authService: AuthService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('uid'));
+      if (id) {
+        this.authService.setUser(id);
+        this.loadUserData(id);
+      }
+    });
+  }
+
+  ngOnChanges() {
+    const uid = this.route.snapshot.paramMap.get('uid');
+    if (uid) {
+      this.loadUserData(+uid);
+    }
+  }
+
+  loadUserData(uid: number): void {
+    const userData = this.authService.getUserById(uid);
+    if (userData) {
+      this.user = userData;
+
+      if (this.authService.isDoctor()) {
+        this.finishedDocuments = [
+          { name: 'Allergies' },
+        ];
+        this.myDocuments = [
+          { name: 'Lab analysis' },
+          { name: 'Prescription' },
+        ];
+      } else {
+        this.finishedDocuments = [
+          { name: 'Lab analysis' },
+          { name: 'Prescription' },
+        ];
+        this.myDocuments = [
+          { name: 'Allergies' },
+        ];
+      }
+    }
+  }
 }
