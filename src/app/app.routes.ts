9c27b2a3fae6a0a166796faa8b0eaefe83cd6178
adnaja01 <adnaja011@gmail.com>
2025-04-10 03:13:36 +0200

    My Patients page for doctors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { RegisterComponentComponent } from './register-component/register-component.component';
+import {PatientsListComponent} from './patients/patients-list/patients-list.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponentComponent },
   { path: 'register', component: RegisterComponentComponent },
   { path: 'user/:uid', component: ProfileComponent },
-  { path: 'documents/:uid', component: DocumentsComponent }
+  { path: 'documents/:uid', component: DocumentsComponent },
+  { path: 'my-patients/:uid', component: PatientsListComponent }
 ];
 
 
