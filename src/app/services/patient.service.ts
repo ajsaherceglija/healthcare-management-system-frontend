e5d065d50b1f3db15b6eea060f71bc994f22ffb8
ajsaherceglija <ajsa.herceglija@gmail.com>
2025-05-30 16:02:13 +0200

    connect appointments to backend

---
+++ src/app/services/patient.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { UserDto } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PatientService {
+  private baseUrl = 'http://localhost:8080';
+
+  // Optional: keep selected patient in BehaviorSubject for sharing across components
+  private selectedPatientSubject = new BehaviorSubject<UserDto | null>(null);
+  selectedPatient$ = this.selectedPatientSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  // Fetch patient by ID from backend
+  getPatientById(uid: number): Observable<UserDto> {
+    return this.http.get<UserDto>(`${this.baseUrl}/user/${uid}`);
+  }
+
+  setPatient(patient: UserDto) {
+    this.selectedPatientSubject.next(patient);
+  }
+
+  getPatient(): UserDto | null {
+    return this.selectedPatientSubject.getValue();
+  }
+
+  clearPatient() {
+    this.selectedPatientSubject.next(null);
+  }
+}
