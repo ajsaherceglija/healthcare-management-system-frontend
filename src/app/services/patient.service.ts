a076b6b8cd71beef4d3328dbf293c8f3880e0bc3
ajsaherceglija <ajsa.herceglija@gmail.com>
2025-05-31 01:07:23 +0200

    connect to heroku

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -7,7 +7,7 @@ import { UserDto } from '../models/user.model';
   providedIn: 'root',
 })
 export class PatientService {
-  private baseUrl = 'http://localhost:8080';
+  private baseUrl = 'https://healthcare-management-system-04601a8f10ae.herokuapp.com/';
 
   // Optional: keep selected patient in BehaviorSubject for sharing across components
   private selectedPatientSubject = new BehaviorSubject<UserDto | null>(null);
